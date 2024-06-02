c679ba05c9f2de203bced2648c5cd8fc8c26663f
Mr. Benny <64999654+bnguy101@users.noreply.github.com>
2024-06-01 19:24:55 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_budgets_budget" "aws-no-make-me-broke-pls" {
+  name              = "monthly-budget"
+  budget_type       = "COST"
+  limit_amount      = "20.0"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+  time_period_start = "2024-05-20_00:00"
